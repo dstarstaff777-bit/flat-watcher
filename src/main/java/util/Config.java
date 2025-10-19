57893a945188e689f0d226eb45c07d22d3ed9b51
Валерий <d.starstaff777@gmail.com>
2025-10-19 22:00:30 +0300

    Changes 1

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -8,32 +8,23 @@ public class Config {
     private static final Properties properties = new Properties();
 
     static {
-        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+        try {
+            InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties");
             if(input != null) {
                 properties.load(input);
             } else {
-                throw new RuntimeException("Unable to find config.properties");
+                System.err.println("Файл не найден в ресурсах");
             }
+            System.getenv().forEach((k, v) -> {
+                if(k.startsWith("TELEGRAM_") ||  k.startsWith("AVITO_")) {
+                    properties.setProperty(k.toLowerCase().replace('_', '.'), v);
+                }
+            });
         } catch (IOException e) {
             throw new RuntimeException("Error loading configuration", e);
         }
     }
     public static String getProperty(String key) {
         return properties.getProperty(key);
-    }
-    public static int getIntProperty(String key) {
-        return Integer.parseInt(properties.getProperty(key));
-    }
-    public static long getLongProperty(String key) {
-        return Long.parseLong(properties.getProperty(key));
-    }
-    public static int getTargetPrice() {
-        return Integer.parseInt(getProperty("target.price"));
-    }
-    public static int getCheckIntervalMinutes() {
-        return Integer.parseInt(getProperty("check.interval.minutes"));
-    }
-    public static String getAvitoUrl() {
-        return getProperty("avito.url");
     }
 }
