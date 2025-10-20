0a2588b7b81d254c6f29700fc63a2b315dd16f3a
Валерий <d.starstaff777@gmail.com>
2025-10-20 21:03:14 +0300

    Changes 4

--- src/main/java/util/SeleniumFetcher.java
+++ src/main/java/util/SeleniumFetcher.java
@@ -1,9 +1,15 @@
 package util;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SeleniumFetcher {
     public static String fetchPageSource(String url) {
@@ -19,14 +25,15 @@ public class SeleniumFetcher {
 
         WebDriver driver = new ChromeDriver(options);
         try {
-            driver.get(url);
-            Thread.sleep(5000);
-            return driver.getPageSource();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            driver.quit();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.presenceOfElementLocated(
+                    By.cssSelector("span[class*=price]")));
+        } catch (TimeoutException e) {
+            System.out.println("Не удалось дождаться загрузки цены для " + url);
+        }
+        String html = driver.getPageSource();
+        driver.quit();
+        return html;
         }
     }
 }
